7fad4bc73f077ec549e914744a66c6dec3b69c87
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-02-27 23:56:22 +0530

    add first view(FBV)

--- chapter-2/pages/views.py
+++ chapter-2/pages/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+#Technically there are 3 different ways to write a view in django
+# 1. CLass-Based views
+# 2. Function-Based views
+# 3. Generic Class-Based views
+def homePageView(request):
+    return HttpResponse("Hello, world!")
